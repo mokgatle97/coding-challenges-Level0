4d3ae73c4ee1aa9810579d13e7073680e6061852
Lefa Mokgatle <mokgatlelefa@gmail.com>
2021-09-16 14:18:58 +0200

    'task modified'

--- task-10.js
+++ task-10.js
@@ -1,21 +1,21 @@
 'use strict'
 
 const commonLetters = function (string1, string2) {
-    let commonWords = [];
+    let repeatedLetters = [];
     let s1 = string1.split('');
     let s2 = string2.split('');
     if(s1.length  > s2.length) {
         for(let i = 0; i < s1.length; i ++) {
             if(s2.includes(s1[i])){
-                commonWords.push(s1[i]);
+                repeatedLetters.push(s1[i]);
             }
         }
     } else (s2.length > s1.length); {
         for(let i = 0; i < s2.length; i ++) {
             if(s1.includes(s2[i])){
-                commonWords.push(s2[i]);
+                repeatedLetters.push(s2[i]);
             }
         }
     }
-    return commonWords.join(',');
+    return `From the 2 strings, the common letters are ${repeatedLetters.join(',')}`;
 }
