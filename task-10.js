01786c2d035146f96c17e3a5030311ded4036c1b
mokgatle97 <87324369+mokgatle97@users.noreply.github.com>
2021-09-20 11:05:10 +0200

    Update task-10.js

--- task-10.js
+++ task-10.js
@@ -1,21 +1,25 @@
 'use strict'
 
 const commonLetters = function (string1, string2) {
-    let repeatedLetters = [];
-    let s1 = string1.split('');
-    let s2 = string2.split('');
-    if(s1.length  > s2.length) {
-        for(let i = 0; i < s1.length; i ++) {
-            if(s2.includes(s1[i])){
-                repeatedLetters.push(s1[i]);
-            }
-        }
-    } else (s2.length > s1.length); {
-        for(let i = 0; i < s2.length; i ++) {
-            if(s1.includes(s2[i])){
-                repeatedLetters.push(s2[i]);
-            }
-        }
+  let repeatedLetters = [];
+  let s1 = string1.split("");
+  let s2 = string2.split("");
+  if (s1.length > s2.length) {
+    for (let i = 0; i < s1.length; i++) {
+      if (s2.includes(s1[i])) {
+        repeatedLetters.push(s1[i]);
+      }
     }
-    return `From the 2 strings, the common letters are ${repeatedLetters.join(',')}`;
-}
+  } else s2.length > s1.length;
+  {
+    for (let i = 0; i < s2.length; i++) {
+      if (s1.includes(s2[i])) {
+        repeatedLetters.push(s2[i]);
+      }
+    }
+  }
+  console.log("Common Letters: " + repeatedLetters.join(", "));
+};
+
+commonLetters();
+
