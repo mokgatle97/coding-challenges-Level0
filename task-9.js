5aeba28967e10ab03b570e31410799c9a6451f85
mokgatle97 <87324369+mokgatle97@users.noreply.github.com>
2021-09-20 10:57:13 +0200

    Update task-9.js

--- task-9.js
+++ task-9.js
@@ -1,14 +1,18 @@
 'use strict'
 
 const vowels = function (string) {
-    let letters = string.split('');
-    let lettersCount = [];
-    let vowelLetters = ['a', 'e','i','o','u', 'A','E', 'I', 'O', 'U'];
-    for (let i = 0; i < letters.length; i++){
-        if(vowelLetters.includes(letters[i])) {
-        lettersCount.push(letters[i]);
-     }
+  let letters = string.split("");
+  let lettersCount = [];
+  let vowelLetters = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  for (let i = 0; i < letters.length; i++) {
+    if (
+      vowelLetters.includes(letters[i]) &&
+      lettersCount.indexOf(letters[i]) === -1
+    ) {
+      lettersCount.push(letters[i]);
     }
-    return lettersCount.join(', ');
+  }
+  console.log("Vowels: " + lettersCount.join(", "));
 };
+vowels();
 
