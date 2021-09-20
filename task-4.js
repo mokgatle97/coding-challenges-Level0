80955d65f82d18c956bd579f5a01d05cd726ae9a
mokgatle97 <87324369+mokgatle97@users.noreply.github.com>
2021-09-20 10:31:47 +0200

    Update task-4.js

--- task-4.js
+++ task-4.js
@@ -1,9 +1,10 @@
 'use strict'
 // Task 0.4
 const evenOrOdd = function (integer) {
-    if(Number(integer) % 2 == 0) {
-        return `${integer} is even!`
-    }else {
-        return `${integer} is odd!`
-    }
+  if (Number(integer) % 2 == 0) {
+    console.log("Even!");
+  } else {
+    console.log("Odd!");
+  }
 };
+evenOrOdd();
